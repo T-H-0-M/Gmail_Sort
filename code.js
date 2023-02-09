e7580d0735ca971c1c9180e0e83b208844c1a555
Thomas <thomasbandy23@gmail.com>
2023-02-09 20:31:35 +1100

    Added getThreads for email and header.

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 
-
+// Retreives up to 250 emails matching the sender email parsed
 function getThreads(email){
     var max_threads = 250;
     var complete = false;
@@ -11,4 +11,33 @@ function getThreads(email){
         }
     }
     return threads;
+}
+
+// Retreives up to 250 emails matching the parsed header
+function getThreads(header){
+    var max_threads = 250;
+    var complete = false;
+    var threads = [];
+    while(!complete){
+        threads = GmailApp.search('subject:' + header, threads.length, threads.length + 50);
+        if(threads.length == max_threads){
+            complete = true;
+        }
+    }
+    return threads;
+}
+
+// Adds the parsed tag to the parsed email and moves it to archive
+function addTag(email, tag){
+    var threads = getThreads(email);
+    var label = GmailApp.getUserLabelByName(tag);
+    if(threads.length == 1){
+        label.addToThread(threads[0]);
+    }
+    else if(threads.length > 1){
+        label.addToThreads(threads);
+    }
+    for(i = 0; i < threads.length; i++){
+        threads[i].moveToArchive();
+    }
 }
